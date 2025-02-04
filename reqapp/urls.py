c33b46f264eb0f564f69640357ed311df5a3494e
Dmitry <dmitry.dima-sol@yandex.ru>
2025-02-04 21:51:16 +0300

    base version 1.0
    hello commit

--- reqapp/urls.py
+++ reqapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login_view, create_request, request_list, logout_view
+from .views import home_view, login_view, create_request, request_list, logout_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('login/', login_view, name='login'),
     path('create_request/', create_request, name='create_request'),
     path('requests/', request_list, name='request_list'),
--- reqapp/views.py
+++ reqapp/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import AuthenticationForm
 from .forms import RequestForm
 from .models import Request, Device
 
+def home_view(request):
+    return render(request, 'home.html')
+
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
