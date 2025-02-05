cc7065336edd9dbe35a05302599787039cc0c99c
Dmitry <dmitry.dima-sol@yandex.ru>
2025-02-05 09:51:03 +0300

    add category for device
    hello commit

--- reqapp/admin.py
+++ reqapp/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Request, Device
+from .models import Request, Device, Category
 
 class RequestAdmin(admin.ModelAdmin):
     list_display = ('user', 'device', 'description', 'status', 'created_at')
     list_filter = ('status', 'created_at')
     search_fields = ('description', 'user__username', 'device__name')
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Request, RequestAdmin)
 admin.site.register(Device)
--- reqapp/models.py
+++ reqapp/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Device(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='devices')
+    code = models.CharField(max_length=100, blank=True, null=True, unique=True)
 
     def __str__(self):
         return self.name
